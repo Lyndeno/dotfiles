18220ade6d4d4a23e5671777c2c02194375cbbc7
Lyndon Sanche <lsanche@lyndeno.ca>
2021-01-18 17:37:33 -0700

    Add glyph to update notification

--- .config/waybar/scripts/checkupdates.sh
+++ .config/waybar/scripts/checkupdates.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+notify-send " Package Updates" "Checking for updates..."
 no_updates=$(checkupdates | wc -l)
 
 if (("$no_updates" == "0"))
 then
     exit 0
 else
-    notify-send "Package Updates" "$no_updates updates are available"
+    notify-send " Package Updates" "$no_updates updates are available for install, run 'paru' to update."
     echo $no_updates
 fi
