ceb081a488bf1baa5433d545f31d55961d6dca62
Lyndon Sanche <lsanche@lyndeno.ca>
2021-01-26 00:03:32 -0700

    Created checkdots script

---
+++ .config/waybar/scripts/checkdots.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+notify_title=" Dotfiles"
+notify-send "$notify_title" "Checking for updates..."
+yadm fetch
+no_updates=$(yadm status -s | wc -l)
+
+if (("$no_updates" == "0"))
+then
+    notify-send "$notify_title" "Dotfiles up to date"
+    exit 0
+elif (("$no_updates" == "1"))
+then
+    notify-send "$notify_title" "$no_updates change ready, ready to push or pull."
+    echo $no_updates
+else
+    notify-send "$notify_title" "$no_updates changes ready, ready to push or pull."
+    echo $no_updates
+fi
