2011d0c6908ff09ba64302dab78055fe4eaed2a3
Lyndon Sanche <lsanche@lyndeno.ca>
2021-03-16 20:40:55 -0600

    Automatic monitor configuration for the laptop

--- .local/bin/mv-workspace.sh
+++ .local/bin/mv-workspace.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+display=$1
 current=$(swaymsg -pt get_workspaces | grep focused | awk '{ print $2 }')
-swaymsg workspace "$1", move workspace to "$2"
+shift
+for workspace in "$@"
+do
+    swaymsg workspace "$workspace", move workspace to "$display"
+done
 swaymsg workspace $current
