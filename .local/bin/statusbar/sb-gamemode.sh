9aa9494cf8459ca1f8c9f127311bb4e2c70009a7
Lyndon Sanche <lsanche@lyndeno.ca>
2021-08-19 12:32:22 -0600

    Make statusbar gamemode script not output when gamemode doesn't exist

--- .local/bin/statusbar/sb-gamemode.sh
+++ .local/bin/statusbar/sb-gamemode.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+# gamemode is not always needed
+if ! command -v gamemode &> /dev/null
+then
+	exit 0
+fi
+
 gm_status=$(gamemoded -s)
 
 if [ "$gm_status" == "gamemode is inactive" ]
